17a8a2387feaae7844b0a191883729364e39655e
idevtier <idevtier@gmail.com>
2022-08-03 18:33:44 +0200

    Adding cht.sh

--- cht.sh
+++ cht.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-languages=`echo "javascript nodejs python" | tr ' ' '\n'`
-core_utils=`echo "awk find mv tar" | tr ' ' '\n'`
+languages=`echo "js python java" | tr ' ' '\n'`
 
-selected=`printf "$languages\n$core_utils" | fzf`
+selected=`printf "$languages" | fzf`
 read -p "query: " query
 
-
-if printf $languages | grep -qs $selected; then
-    query=`echo $query | tr ' ' '+'`
-    echo "lang: $selected"
-    echo "cht.sh/$selected/$query"
-    tmux neww bash -c "curl cht.sh/$selected/$query & while :; do sleep 1; done"
-else
-    # tmux neww bash -c curl curl cht.sh/$selected~$query & while :; do sleep 1; done`
-    tmux neww bash -c "curl cht.sh/$selected~$query & while [:]; do sleep 1; done"
-fi
+query=`echo $query | tr ' ' '+'`
+tmux neww bash -c "curl cht.sh/$selected/$query | batcat"
