9e75b4359be901293eb9837960b488b2183334e3
idevtier <idevtier@gmail.com>
2022-12-13 19:50:07 +0300

    Add bookmarks to search_in_browser

---
+++ search_in_browser.py
@@ -0,0 +1,66 @@
+import sys
+import subprocess
+from collections import defaultdict
+from typing import Optional
+from pyfzf.pyfzf import FzfPrompt
+
+
+Link = str
+Browser = str
+Query = Link | str
+
+
+SEARCH_QUERY = "https://duckduckgo.com/?q={query}&t=ffab&ia=web"
+
+
+class Bookmarks:
+    _data: defaultdict[str, Link]
+
+    def __init__(self) -> None:
+        self._data = defaultdict(lambda: None)
+
+    def get_link_by_bookmark(self, bookmark: str) -> Optional[Link]:
+        return self._data[bookmark]
+
+    def add_bookmark(self, bookmark: str, link: Link) -> None:
+        self._data[bookmark] = link
+
+    def get_bookmarks_list(self) -> tuple[str]:
+        return tuple(self._data.keys())
+
+
+def load_bookmarks(bookmarks_path: str) -> Bookmarks:
+    bookmarks = Bookmarks()
+    with open(bookmarks_path, "r") as f:
+        for line in f.readlines():
+            name, link = line.replace("\n", "").split(" - ")
+            bookmarks.add_bookmark(name, link)
+    return bookmarks
+
+
+def load_query(bookmarks: Bookmarks) -> Query:
+    fzf = FzfPrompt()
+    result = fzf.prompt(bookmarks.get_bookmarks_list(), fzf_options="--border rounded --print-query")
+    query = result[0]
+    if len(result) == 1:
+        return SEARCH_QUERY.format(query=query)
+    else:
+        return bookmarks.get_link_by_bookmark(query)
+
+
+def open_link_in_browser(browser: Browser, query: Query) -> None:
+    subprocess.run([browser, query])
+
+
+def main(browser: Browser, bookmarks_path: str) -> None:
+    bookmarks = load_bookmarks(bookmarks_path)
+    query = load_query(bookmarks)
+    print(f"{query=}")
+    open_link_in_browser(browser, query)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Pass browser and path to bookmarks as args")
+        exit(1)
+    main(sys.argv[1], sys.argv[2])
--- search_in_firefox.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo -n "Enter your query: "
-read query
-firefox $query
