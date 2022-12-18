6ef5e6d176ccec9911825db626deda78cc3cb7fb
idevtier <idevtier@gmail.com>
2022-12-18 17:10:40 +0300

    add cava visualisation to music player

--- music_player.sh
+++ music_player.sh
@@ -5,6 +5,11 @@ window=${session_name}:0
 
 tmux kill-session -t $session_name
 tmux new -s $session_name -d
-tmux send-keys -t music "python3 ~/.config/tmux/music_player_core.py" Enter
+tmux send -t music:1 "python3 ~/.config/tmux/music_player_core.py" Enter
 tmux switch -t $session_name
-
+if command -v cava &> /dev/null
+then
+    tmux split-window -t music:.1
+    tmux select-pane -t music:.1
+    tmux send -t music:.2 'cava' Enter
+fi
